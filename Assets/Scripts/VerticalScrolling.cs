f3002d398805b531319f1a15c84cf755db46992c
gZegoudia <guillaume.zegoudia@gmail.com>
2017-04-11 20:10:57 +0200

    debut scrolling + dezbut LD

---
+++ Assets/Scripts/VerticalScrolling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScrolling : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject GamePlane;
+    [SerializeField]
+    private GameObject Background;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Background.transform.position += new Vector3(0,  Time.deltaTime * 0.7f, 0);
+        GamePlane.transform.position += new Vector3(GamePlane.transform.position.x,  Time.deltaTime, GamePlane.transform.position.z);
+		
+	}
+}
