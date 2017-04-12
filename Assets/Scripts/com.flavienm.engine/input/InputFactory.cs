c1be878bf02e04c232e28039b93b76e83f40f199
gZegoudia <guillaume.zegoudia@gmail.com>
2017-04-12 10:35:10 +0200

    resolution erreur lancement InputTobbi dans l'inspecteur

--- Assets/Scripts/com.flavienm.engine/input/InputFactory.cs
+++ Assets/Scripts/com.flavienm.engine/input/InputFactory.cs
@@ -18,13 +18,15 @@ namespace com.flavienm.engine.input
 
         private static void CreateInputObject(bool tobii)
         {
+            if(tobii)
                 GameObjectUtils.CreateGameObjectWithScript<InputTobii> ("InputTobii");
-           
+           else
                 GameObjectUtils.CreateGameObjectWithScript<InputDesktop>("InputDesktop");
         }
 
         private static bool hasEyeTracking ()
         {
+            EyeTracking.Initialize();
             Debug.Log(EyeTrackingHost.GetInstance().UserPresence.IsUserPresent);
             Debug.Log(EyeTracking.GetGazeTrackingStatus().IsTrackingEyeGaze);
             return
