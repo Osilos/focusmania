b497af43abce5817e528ab22cbc671553c545113
Naowin <nathan_bardin@hotmail.com>
2017-04-14 11:42:44 +0200

    danse

---
+++ Assets/Scripts/PigeonSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonSpawner : MonoBehaviour {
+
+	public GameObject GOPigeon;
+
+	private Coroutine currentCoroutine;
+
+	private void Start()
+	{
+		if (currentCoroutine != null)
+		{
+			currentCoroutine = StartCoroutine(LaunchPigeon());
+		}
+	}
+
+	IEnumerator LaunchPigeon()
+	{
+		yield return new WaitForSeconds(5 + Random.Range(0f, 4f));
+		GameObject pigeon = Instantiate(GOPigeon);
+		pigeon.transform.position = transform.position + new Vector3(0, Random.Range(-GetComponent<Collider>().bounds.size.y / 2, GetComponent<Collider>().bounds.size.y / 2), 0);
+	}
+}
+
